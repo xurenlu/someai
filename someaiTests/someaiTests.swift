cd460c25c55b9d8df538023d0ee35dd0f50c12ed
rocky <m@some.im>
2026-02-18 19:57:14 +0800

    Initial Commit

---
+++ someaiTests/someaiTests.swift
@@ -0,0 +1,17 @@
+//
+//  someaiTests.swift
+//  someaiTests
+//
+//  Created by rocky on 2026/2/18.
+//
+
+import Testing
+@testable import someai
+
+struct someaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
